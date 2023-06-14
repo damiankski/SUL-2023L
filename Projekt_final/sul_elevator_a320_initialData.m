52ffb1c50bbb894e75bead99ac7262e2600e52d6
lukaszk22 <lukaszkicinski22@gmail.com>
2023-06-14 02:21:50 +0200

    uporzadkowanie kodu i zmiany w raporcie

--- Projekt_final/sul_elevator_a320_initialData.m
+++ Projekt_final/sul_elevator_a320_initialData.m
@@ -4,38 +4,35 @@ close; %zamyka wszystkie okna
 %% Geometria i geometria odwrotna
 
 % Deklaracja danych
-y_max = 100;
-L = 180;
-R=100;
-Y=180;
+L = 180;    % mm
+R=100;      % mm
+Y=180;      % mm
 alpha=asin(R/(2*L));
 alpha_prim=pi/2 - alpha;
 delta_max = 25;
 
 % określenie wektora wychylenia steru
-delta=-25:0.1:25;
+delta=-delta_max:0.1:delta_max;
 
 % zamiana wychyelnia steru na wysunięcie siłownika
 delta_h = -(Y-sqrt(L^2+R^2-2*L*R*cos(alpha_prim-delta*pi/180)));
 
 % Wykres wydłużenia siłownika w funkcji kąta wychylenia steru
-plot(delta,delta_h);
+plot(delta,delta_h)
 grid on
 xlabel('\delta [\circ]');
 ylabel('\delta_h [mm]');
 grid on
-close
 
+close %zamyka okno z wykresem
 %% Dynamika układu
 % Deklaracja danych
 
-v_dzwieku = 340.3;
-Ma= 0.3;
-v_lotu = v_dzwieku*Ma;
-c = 2.37;
-rho = 1.2255;
-b = 12.45;
-wsp1 = 0.5*rho * c^2 * b;
+v_dzwieku = 340.3;         % m/s
+c = 2.37;                  % m
+rho = 1.2255;              % kg/m^3
+b = 12.45;                 % m
+wsp1 = 0.5*rho * c^2 * b;  % współczynnik wymiarujący moment zawiasowy-
 
 % Określenie wektora dopuszczalnych prędkości
 V = (0.1*v_dzwieku:10:0.8*v_dzwieku)';
@@ -50,18 +47,32 @@ M_h = wsp1*cm_h;
 delta_h_dyn = -(Y-sqrt(L^2+R^2-2*L*R.*cos(alpha_prim-delta_dyn)));
 beta = acos((R^2 +delta_h_dyn.*(delta_h_dyn+2*L))./(2*R*(L+delta_h_dyn))) - pi/2;
 R_B = M_h./(R*cos(beta));
-    
-Q = V.^2*R_B;
+
+ % wyjściowa tablica do modelu w Simulink
+Q = V.^2*R_B;     
+
+% utworzenie siatki pod wizualizację 3D
+[X,Y] = meshgrid(delta_dyn, V);   
+% uwtorzenie wartości powierzchni
+Z = M_h./(R*cos(acos((R^2 +delta_h_dyn.*(delta_h_dyn+2*L))./(2*R*(L+delta_h_dyn))) - pi/2)).*V.^2;
+
+surf(rad2deg(X),Y,Z)
+xlabel('\delta [\circ]')
+ylabel('V [m/s]')
+zlabel('R_B [N]')
+grid on
+
+close %zamyka okno z wykresem
 
 %Wykres znormalizowanego obciążenia w funkcji wychylenia steru
-plot(delta_dyn,R_B,'LineWidth',2);
+figure
+plot(rad2deg(delta_dyn),R_B,'LineWidth',2);
 grid on
 xlabel('\delta [\circ]');
-ylabel('R_B/v^2 [N/(m/s)^2]');
-axis([-pi/6 pi/6 -0.02 0.02])
-grid on
-close
+ylabel('\it{R_B}/v^2 \it{[N/(m/s)^2]}');
+axis([-30 30 -0.02 0.02])
 
+close %zamyka okno z wykresem
 %% Siłownik
 
 % Deklaracja danych
@@ -71,7 +82,7 @@ m = 10;               % kg
 K = 1.8e5;            % Pa
 V_1 = 2.26*1e-4;      % m^3
 V_2 = 6.68*1e-5;      % m^3
-b_tlum = -5;               % kg/s
+b_tlum = -5;          % kg/s
 
 % warunki początkowe
 p_01 = 1e6;           % Pa
@@ -79,7 +90,8 @@ p_02 = p_01*A_1/A_2;  % Pa
 
 x_0 = 0;              % m
 xp_0 = 0;             % m/s
-
-%% Dane wejściowe do symulacji
+%% Dane wejściowe
 
 v_lotu = 0.5*340;     % m/s
+kat_wychylenia =7;   % stopnie
+czas_wymuszenia = 8;  % s
